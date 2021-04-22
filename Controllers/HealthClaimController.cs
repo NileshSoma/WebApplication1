0fb0e80c5227ae65554b95a81c303fbc0df54ac7
nilesh.soma123 <nilesh.soma123@gmail.com>
2021-04-22 11:43:17 +0530

    Add project files.

---
+++ Controllers/HealthClaimController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using LumenWorks.Framework.IO.Csv;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthClaimController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("[action]")]
+        [Route("api/HealthClaim/GetClaims")]
+        public IActionResult GetClaims()
+        {
+
+            Claims obj = new Claims();
+            Members objmembers = new Members();
+            try
+            {
+                var item = from Claims in obj.BindClims()
+                           join Memebers in objmembers.BindMembers()
+                           on Claims.MemberID equals Memebers.MemberID
+                           select new { Claims.MemberID, Claims.ClaimDate, Claims.claimammount, Memebers.FirstName, Memebers.LastName };
+                if (item != null)
+                {
+                    return Ok(item);
+                }
+                else if (item == null)
+                {
+                    return NotFound(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+            return NotFound("Not found");
+        }
+
+        [HttpGet]        
+        [Route("/GetByClaimDate/{ClaimDate}")]
+        public IActionResult GetByClaimDate(string ClaimDate)
+        {
+
+            Claims obj = new Claims();
+            Members objmembers = new Members();
+            try
+            {
+                var item = from Claims in obj.BindClims()
+                           join Memebers in objmembers.BindMembers()
+                           on Claims.MemberID equals Memebers.MemberID
+                           where Claims.ClaimDate == Convert.ToDateTime(ClaimDate)
+                           select new { Claims.MemberID, Claims.ClaimDate, Claims.claimammount, Memebers.FirstName, Memebers.LastName };
+                if (item != null)
+                {
+                    return Ok(item);
+                }
+                else if (item == null)
+                {
+                    return NotFound(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+            return NotFound("Not found");
+        }
+    }
+}
---
+++ Models/Claim.cs
@@ -0,0 +1,64 @@
+﻿using LumenWorks.Framework.IO.Csv;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+
+    public class Claims
+    {
+
+        public int MemberID { get; set; }
+        public DateTime? ClaimDate { get; set; }
+        public decimal? claimammount { get; set; }
+
+        public List<Claims> BindClims()
+        {
+            var csvClaimsTable = new DataTable();
+            List<Claims> objClaims = new List<Claims>();
+            using (var csvclaimesReader = new CsvReader(new StreamReader(System.IO.File.OpenRead(@"D:\1\New folder (2)\WebApplication1\Services\Claim.csv")), true))
+            {
+                csvClaimsTable.Load(csvclaimesReader);
+            }
+            for (int i = 0; i < csvClaimsTable.Rows.Count; i++)
+            {
+                objClaims.Add(new Claims { MemberID = Convert.ToInt32(csvClaimsTable.Rows[i][0]), ClaimDate = Convert.ToDateTime(csvClaimsTable.Rows[i][1].ToString()), claimammount = Convert.ToDecimal(csvClaimsTable.Rows[i][2]) });
+            }
+            
+            return objClaims;
+        }
+    }
+
+    public class Members
+    {
+        public int MemberID { get; set; }
+        public DateTime? EnrollmentDate { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public List<Members> BindMembers()
+        {
+            List<Members> objEnrolementes = new List<Members>();
+            var csvEnroleTable = new DataTable();
+            
+            using (var csvEnroleReader = new CsvReader(new StreamReader(System.IO.File.OpenRead(@"D:\1\New folder (2)\WebApplication1\Services\Member.csv")), true))
+            {
+                csvEnroleTable.Load(csvEnroleReader);
+            }
+            
+            for (int i = 0; i < csvEnroleTable.Rows.Count; i++)
+            {
+                objEnrolementes.Add(new Members { MemberID = Convert.ToInt32(csvEnroleTable.Rows[i][0]), EnrollmentDate = DateTime.Parse(csvEnroleTable.Rows[i][1].ToString()) , LastName = csvEnroleTable.Rows[i][3].ToString(), FirstName = csvEnroleTable.Rows[i][2].ToString() });
+            }
+            return objEnrolementes;
+        }
+    }
+
+    
+
+
+}
